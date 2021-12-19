7132620ee22bae8329a8b0b7efa9c900979b54df
unknown <md.ridgeway.work@gmail.com>
2021-12-19 21:54:18 +0100

    removed comments

--- contracts/DAORoom.sol
+++ contracts/DAORoom.sol
@@ -1,33 +1,33 @@
-//SPDX-License-Identifier: GPL-3.0-only
+SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
-// contract DAORoom {
+contract DAORoom {
 
-//     address payable public owner;
+    address payable public owner;
 
-//     uint public cost;
+    uint public cost;
 
-//     enum Status {Vacant, Occupied}
+    enum Status {Vacant, Occupied}
 
-//     Status currentStatus;
+    Status currentStatus;
 
-//     event Occupied(address _occupant);
+    event Occupied(address _occupant);
 
-//     constructor() {
-//         owner =  payable(msg.sender);
-//         cost = 1;
-//         currentStatus = Status.Vacant;
-//     }
+    constructor() {
+        owner =  payable(msg.sender);
+        cost = 1;
+        currentStatus = Status.Vacant;
+    }
 
-//     modifier onlyWhenVacant {
-//         require(currentStatus == Status.Vacant, "Room is occupied");
-//         _;
-//     }
+    modifier onlyWhenVacant {
+        require(currentStatus == Status.Vacant, "Room is occupied");
+        _;
+    }
 
-//     function rent() external payable public onlyWhenVacant {
-//         require(msg.value != cost, "Insufficient funds");
-//         owner.transfer(msg.value);
-//         emit Occupied(msg.sender);
-//     }
+    function rent() external payable public onlyWhenVacant {
+        require(msg.value != cost, "Insufficient funds");
+        owner.transfer(msg.value);
+        emit Occupied(msg.sender);
+    }
 
-// }
+}
